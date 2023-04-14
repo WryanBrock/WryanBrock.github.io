5838dedd97ebe7ca5dc21e0573f986943abb2a8d
Wryan Brock Ledbetter <wryanbrock@gmail.com>
2023-04-13 20:07:24 -0500

    working on the responsive nav menuToggle

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
 var myVar;
 
 function myFunction() {
-  myVar = setTimeout(showPage, 4000);
+  myVar = setTimeout(showPage, 3000);
 }
 
 function showPage() {
 //   document.getElementById("loader").style.display = "none";
   document.getElementById("show").style.display = "block";
+}
+
+
+let list = document.querySelectorAll('.nav li');
+function active(){
+  list.forEach((i)=>
+  i.classList.remove('active'));
+  this.classList.add('active');
+}
+list.forEach((i)=>
+i.addEventListener('click', active));
+
+let menuToggle = document.querySelector('.menuToggle');
+let header = document.querySelector('header');
+menuToggle.onClick = function(){
+  header.classList.toggle('active');
 }
